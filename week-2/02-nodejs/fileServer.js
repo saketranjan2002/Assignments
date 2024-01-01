d324f44d85247bbf4e0e05cc1231156f80d1b937
Saket Ranjan <saketranjan2002@gmail.com>
2024-01-01 16:35:52 +0530

    middlewares,global catch,zod added

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -19,9 +19,34 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
+const zod = require('zod');
+
+// {
+//   msg:
+//   pass: atleast 8 char,
+//   country: IN or US  
+//   arr: [1,2,2] 
+// }
+
+const tempschema = zod.object({
+  msg: zod.string(),
+  password : zod.string(),
+  country: zod.literal('IN').or(zod.literal('US')),
+  arr: zod.array(zod.number())
+})
+
 app.listen(PORT,(req,res)=>{{
   console.log(`server running : http://localhost:${PORT}`);
 }})
+
+// global middleware
+// eg express.json()
+// will be called in every route
+app.use(express.json());
+app.use((req,res,next)=>{
+
+})
+
 
 app.get("/",(req,res)=>{
   res.send("<h1>Home Page</h1>")
@@ -38,16 +63,26 @@ app.get("/files",(req,res)=>{
 
 
 app.get("/files/:x",(req,res)=>{
-  const filepath = path.join(__dirname,"./files/",req.params.x);
-  fs.readFile(filepath,'utf-8',(err,data)=>{
-    if(err){
-      return res.status(404).send("file not found");
-    }
-    res.send(data);
-  })
+  const schema = zod.string();
+  const response = schema.safeParse(req.params.x);
+  if(response.success){
+    const filepath = path.join(__dirname,"./files/",req.params.x);
+    fs.readFile(filepath,'utf-8',(err,data)=>{
+      if(err){
+        return res.status(404).send("file not found");
+      }
+      res.send(data);
+    })
+  }else{
+    res.send("invalid input");
+  }
 })
 
 
+// global catch
+app.use((err,req,res,next)=>{
+  console.log("sorry something is up with our server");
+})
 
 
 module.exports = app;
